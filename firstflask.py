9610c9f6d2e96306d36e687d10b673571a5a0dde
Neil <yulin.xian@nokia.com>
2017-12-29 01:18:33 +0800

    add edit rule

--- firstflask.py
+++ firstflask.py
@@ -15,7 +15,8 @@ app.config.from_object(config)
 db.init_app(app)
 
 
-rulekey = ['CustomerFeatureWhiteList','CustomerFeatureBlackList','CustomerTopFaultList','CustomerCareFunctionTag','RRUType','R4BBUType','R3BBUType','FSIHBBUType','R2BBUType','UUFFilter','UUFFilterExclusion','KPIFilter','KPIFilterExclusion','CAFilter','CAFilterExclusion','OAMStabFilter','OAMStabFilterExclusion','PETStabFilter','PETStabFilterExclusion','FuncFilter','FuncFilterExclusion','CategorySearchField','CategoryTag','CustomerBBU','CustomerRRU','CustomerTitleKeywordWhiteList','CustomerTitleKeywordBlackList','CustomerPRWhiteList','CustomerPRBlackList','FTComSc']
+rulekey1 = ['customer_feature_white','customer_top_fault','customer_bbu','customer_keyword_white','category_tag','uuf_filter','kpi_filter','ca_filter','oamstab_filter','pet_filter','func_filter','customer_pronto_white','r4bbu']
+rulekey2 = ['customer_feature_black','customer_care_function','customer_rru','customer_keyword_black','category_search_field','uuf_exclusion','kpi_exclusion','ca_exclusion','oamstab_exclusion','pet_exclusion','func_exclusion','customer_pronto_black','r3bbu','ftcomsc']
 
 
 @app.route('/')
@@ -24,7 +25,8 @@ rulekey = ['CustomerFeatureWhiteList','CustomerFeatureBlackList','CustomerTopFau
 def index():
     user_id = session.get('user_id')
     sourcefilelist = Source.query.filter(Source.owner_id == user_id).order_by(desc(Source.id)).all()
-    return render_template('index.html', sources = sourcefilelist)
+    userrulelist = Rule.query.filter(Rule.owner_id == user_id).order_by(desc(Rule.id)).all()
+    return render_template('index.html', sources = sourcefilelist, userrules=userrulelist)
 
 
 @app.route('/data/')
@@ -32,12 +34,132 @@ def index():
 def data():
     return render_template('data.html')
 
-@app.route('/newrule/')
+
+@app.route('/newrule/',methods=['GET','POST'])
 @login_require
 def newrule():
-    rulekey = Rule.__dict__.iterkeys()
-    print rulekey
-    return render_template('newrule.html', key = rulekey)
+    user_id = session.get('user_id')
+    if request.method == 'GET':
+        userrulelist = Rule.query.filter(Rule.owner_id == user_id).order_by(desc(Rule.id)).all()
+        return render_template('newrule.html', key1 = rulekey1, key2 = rulekey2, userrules = userrulelist)
+    else:
+        rulename = request.form.get('rulename')
+        customer = request.form.get('customer')
+        release = request.form.get('release')
+        customer_feature_white = request.form.get('customer_feature_white')
+        customer_feature_black = request.form.get('customer_feature_black')
+        customer_top_fault = request.form.get('customer_top_fault')
+        customer_care_function = request.form.get('customer_care_function')
+        customer_bbu = request.form.get('customer_bbu')
+        customer_rru = request.form.get('customer_rru')
+        customer_keyword_white = request.form.get('customer_keyword_white')
+        customer_keyword_black = request.form.get('customer_keyword_black')
+        category_tag = request.form.get('category_tag')
+        category_search_field = request.form.get('category_search_field')
+        uuf_filter = request.form.get('uuf_filter')
+        uuf_exclusion = request.form.get('uuf_exclusion')
+        kpi_filter = request.form.get('kpi_filter')
+        kpi_exclusion = request.form.get('kpi_exclusion')
+        ca_filter = request.form.get('ca_filter')
+        ca_exclusion = request.form.get('ca_exclusion')
+        oamstab_filter = request.form.get('oamstab_filter')
+        oamstab_exclusion = request.form.get('oamstab_exclusion')
+        pet_filter = request.form.get('pet_filter')
+        pet_exclusion = request.form.get('pet_exclusion')
+        func_filter = request.form.get('func_filter')
+        func_exclusion = request.form.get('func_exclusion')
+        customer_pronto_white = request.form.get('customer_pronto_white')
+        customer_pronto_black = request.form.get('customer_pronto_black')
+        r4bbu = request.form.get('r4bbu')
+        r3bbu = request.form.get('r3bbu')
+        ftcomsc = request.form.get('func_exclusion')
+
+        rule = Rule.query.filter(Rule.rulename == rulename).first()
+        if rule:
+            return 'Rule is already exist, please try another rule name!'
+        else:
+            rule = Rule(rulename=rulename,customer=customer,release=release,customer_feature_white=customer_feature_white, customer_feature_black=customer_feature_black, customer_top_fault=customer_top_fault, customer_care_function=customer_care_function, uuf_filter=uuf_filter, uuf_exclusion=uuf_exclusion, kpi_filter=kpi_filter, kpi_exclusion=kpi_exclusion, ca_filter=ca_filter, ca_exclusion=ca_exclusion, oamstab_filter=oamstab_filter, oamstab_exclusion=oamstab_exclusion, pet_filter=pet_filter, pet_exclusion=pet_exclusion, func_filter=func_filter, func_exclusion=func_exclusion, category_search_field=category_search_field, category_tag=category_tag, customer_rru=customer_rru, customer_bbu=customer_bbu, customer_keyword_white=customer_keyword_white, customer_keyword_black=customer_keyword_black, customer_pronto_white=customer_pronto_white, customer_pronto_black=customer_pronto_black, r4bbu=r4bbu, r3bbu=r3bbu, ftcomsc=ftcomsc)
+            user = User.query.filter(User.id == user_id).first()
+            rule.owner = user
+            db.session.add(rule)
+            db.session.commit()
+            return redirect(url_for('newrule'))
+
+
+@app.route('/editrule/',methods=['GET','POST'])
+@login_require
+def editrule():
+    user_id = session.get('user_id')
+    if request.method == 'GET':
+        userrulelist = Rule.query.filter(Rule.owner_id == user_id).order_by(desc(Rule.id)).all()
+        return render_template('editrule.html', key1 = rulekey1, key2 = rulekey2, userrules = userrulelist)
+    else:
+        rulename = request.form.get('rulename')
+        customer = request.form.get('customer')
+        release = request.form.get('release')
+        customer_feature_white = request.form.get('customer_feature_white')
+        customer_feature_black = request.form.get('customer_feature_black')
+        customer_top_fault = request.form.get('customer_top_fault')
+        customer_care_function = request.form.get('customer_care_function')
+        customer_bbu = request.form.get('customer_bbu')
+        customer_rru = request.form.get('customer_rru')
+        customer_keyword_white = request.form.get('customer_keyword_white')
+        customer_keyword_black = request.form.get('customer_keyword_black')
+        category_tag = request.form.get('category_tag')
+        category_search_field = request.form.get('category_search_field')
+        uuf_filter = request.form.get('uuf_filter')
+        uuf_exclusion = request.form.get('uuf_exclusion')
+        kpi_filter = request.form.get('kpi_filter')
+        kpi_exclusion = request.form.get('kpi_exclusion')
+        ca_filter = request.form.get('ca_filter')
+        ca_exclusion = request.form.get('ca_exclusion')
+        oamstab_filter = request.form.get('oamstab_filter')
+        oamstab_exclusion = request.form.get('oamstab_exclusion')
+        pet_filter = request.form.get('pet_filter')
+        pet_exclusion = request.form.get('pet_exclusion')
+        func_filter = request.form.get('func_filter')
+        func_exclusion = request.form.get('func_exclusion')
+        customer_pronto_white = request.form.get('customer_pronto_white')
+        customer_pronto_black = request.form.get('customer_pronto_black')
+        r4bbu = request.form.get('r4bbu')
+        r3bbu = request.form.get('r3bbu')
+        ftcomsc = request.form.get('func_exclusion')
+        id = request.form.get('ruleid')
+
+        rule = Rule.query.filter(Rule.id == id).first()
+        if rule:
+            rule.rulename = rulename
+            rule.release = release
+            rule.customer = customer
+            rule.customer_feature_white = customer_feature_white
+            rule.customer_feature_black = customer_feature_black
+            rule.customer_top_fault = customer_top_fault
+            rule.customer_care_function = customer_care_function
+            rule.uuf_filter = uuf_filter
+            rule.uuf_exclusion = uuf_exclusion
+            rule.kpi_filter = kpi_filter
+            rule.kpi_exclusion = kpi_exclusion
+            rule.ca_filter = ca_filter
+            rule.ca_exclusion = ca_exclusion
+            rule.oamstab_filter = oamstab_filter
+            rule.oamstab_exclusion = oamstab_exclusion
+            rule.pet_filter = pet_filter
+            rule.pet_exclusion = pet_exclusion
+            rule.func_filter = func_filter
+            rule.func_exclusion = func_exclusion
+            rule.category_search_field = category_search_field
+            rule.category_tag = category_tag
+            rule.customer_rru = customer_rru
+            rule.customer_bbu = customer_bbu
+            rule.customer_keyword_white = customer_keyword_white
+            rule.customer_keyword_black = customer_keyword_black
+            rule.customer_pronto_white = customer_pronto_white
+            rule.customer_pronto_black = customer_pronto_black
+            rule.r4bbu = r4bbu
+            rule.r3bbu = r3bbu
+            rule.ftcomsc = ftcomsc
+            db.session.commit()
+            return redirect(url_for('editrule'))
 
 
 @app.route('/login/',methods=['GET','POST'])
