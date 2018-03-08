3aced2eb235ec4f56513043c42c61ea5d22daa12
Neil <yulin.xian@gmail.com>
2018-03-08 10:06:15 +0800

    fix figure error

--- firstflask.py
+++ firstflask.py
@@ -13,6 +13,8 @@ from collections import OrderedDict
 import os, datetime, platform, re, xlrd, time, random
 import pandas as pd
 import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 from _mysql import DataError
 import _mysql_exceptions
